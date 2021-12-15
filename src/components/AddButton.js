abd7084af23611cfcf69ce8d0cbd7f2fab4a2cb0
kutlo2019 <kutlopopo@rocketmail.com>
2021-12-15 12:47:42 +0200

    complete header component

---
+++ src/components/AddButton.js
@@ -0,0 +1,11 @@
+import { FaPlusCircle } from 'react-icons/fa'
+
+const AddButton = () => {
+    return (
+        <div style={{ "fontSize": "1.2rem"}} className="mt-1">
+            <FaPlusCircle /> Add
+        </div>
+    )
+}
+
+export default AddButton
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import AddButton from "./AddButton"
+
+const Header = () => {
+
+    const myStyles = {
+        "backgroundColor": "#000",
+        "color": "#fff",
+        "padding": "2rem"
+    }
+    return (
+        <div className="h1 text-center d-flex justify-content-around" style={myStyles}>
+            <h1>Expense Tracker</h1>
+            <AddButton />
+        </div>
+    )
+}
+
+export default Header
